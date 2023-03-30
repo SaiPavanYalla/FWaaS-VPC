fe9b27c3af088b5a11b471507197e60d4ac1e30b
Romika <rjakhar@ncsu.edu>
2023-03-30 17:31:35 +0000

    firewall rule

--- north_bound/firewallPolicies.py
+++ north_bound/firewallPolicies.py
@@ -22,6 +22,15 @@ def is_valid_port(port):
     except ValueError:
         return False
 
+def are_dicts_equal_except_key(d1, d2, key1,key2):
+    d1_copy = d1.copy()
+    d2_copy = d2.copy()
+    d1_copy.pop(key1, None)
+    d1_copy.pop(key2, None)
+    d2_copy.pop(key1, None)
+    d2_copy.pop(key2, None)
+    return d1_copy == d2_copy
+
 firewall_policy_list = []
 
 tenant_name = input("Please enter your Tenant Name")
@@ -35,9 +44,15 @@ if os.path.exists(os.path.join(os.getcwd(), "Network","network.json")):
 
 if not tenant_name in network_data.keys():
     print("Tenant Network is not created. Please create Tenant Network")
+    exit()
 else:
     if not tenant_code == network_data[tenant_name]["tenant_code"] 
         print("Please type the correct Tenant Code")
+        exit()
+    if not ( "Firewall" in network_data[tenant_name].keys() and all(val == "Completed" for val in network_data[tenant_name]["Firewall"]["status"].values())) :
+        print("Please create a Firewall and apply policies to the Tenant Network")
+        exit()
+
 
 def execute_firewall_policy(firewall_policy_obj):
     
@@ -46,16 +61,16 @@ def execute_firewall_policy(firewall_policy_obj):
     inventory_path = os.path.join(parent_dir,"south_bound", "firewall.ini")
     playbook_path = os.path.join(parent_dir,"south_bound", "ansible_scripts","add_fw_rule.yml")
 
-    Src = firewall_policy_obj["src_ip"]
-    dst = firewall_policy_obj["dest_ip"]
-    sport= firewall_policy_obj["src_port"]
-    dport = firewall_policy_obj["dest_port"]
-    protocol = firewall_policy_obj["tcp"]
+    src_ip = firewall_policy_obj["src_ip"]
+    dest_ip = firewall_policy_obj["dest_ip"]
+    src_port= firewall_policy_obj["src_port"]
+    dest_port = firewall_policy_obj["dest_port"]
+    protocol = firewall_policy_obj["protocol"]
     threshold = firewall_policy_obj["threshold"]
     host = network_data[tenant]["namespace_tenant"] + "FW"        
 
     
-    extra_vars = { 'Src': Src ,'dst': dst , 'sport': sport ,'dport': dport, 'protocol': protocol , 'threshold': threshold , 'host': host }
+    extra_vars = { 'src_ip': src_ip ,'dest_ip': dest_ip , 'src_port': src_port ,'dest_port': dest_port, 'protocol': protocol , 'threshold': threshold , 'host': host }
 
     command = ['sudo','ansible-playbook', playbook_path ,'-i', inventory_path]
     sudo_password = "mmrj2023"
@@ -71,18 +86,37 @@ def execute_firewall_policy(firewall_policy_obj):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     stdout, stderr = process.communicate(sudo_password.encode())
 
-    if not "policies" in firewall_data.keys():
-        firewall_data["policies"]= {}
+    if not "Policies" in firewall_data.keys():
+        firewall_data["Policies"]= {}
+
+    flag = True
+    for policy in firewall_data["Policies"]:
+        if are_dicts_equal_except_key(policy,policy_obj,"Status"):
+            flag = False
 
     if process.returncode != 0:
         output = stderr.decode('utf-8') if stderr else stdout.decode('utf-8')
         policy_obj["Status"] = "Failed"
-        firewall_data["policies"].append(policy_obj)
+        
+        if flag = True:        
+            firewall_data["Policies"].append(policy_obj)
         print(f"Ansible playbook failed with error while adding a Policy :\n{output}")
         print(policy_obj)
     else:
         policy_obj["Status"] = "Completed"
-        firewall_data["policies"].append(policy_obj)
+        if flag = True:        
+            firewall_data["Policies"].append(policy_obj)
+        
+
+
+
+
+
+
+#Deleting firewall policies which are not in the requirement file
+
+if "Policies" in network_data[tenant_name]["Firewall"].keys():
+    existing_firewall_policies = network_data[tenant_name]["Firewall"]["keys"]
 
 
 with open('firewallPolicies.csv', 'r') as file:
