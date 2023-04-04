201aa05614975381c715e87f0e153aaeb7e56bba
Romika <rjakhar@ncsu.edu>
2023-04-04 02:18:23 +0000

    some error in firewallPolicies

--- north_bound/firewallPolicies.py
+++ north_bound/firewallPolicies.py
@@ -167,12 +167,7 @@ with open('firewallPolicies.csv', 'r') as file:
                 print(firewall_policy_obj)
                 exit()
 
-        else:
-            if firewall_policy_obj["src_port"] == "any":
-                firewall_policy_obj["src_port"] = "0-65535"
-
-            if firewall_policy_obj["dest_port"] == "any":
-                firewall_policy_obj["dest_port"] = "0-65535"
+       
 
 
         if not (firewall_policy_obj["policy_action"] == "ACCEPT" or firewall_policy_obj["policy_action"] == "DROP" ):
